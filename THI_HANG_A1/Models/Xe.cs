9419fa03a83ce48b68db57c09c20f705858828c1
PhamChinhCode <phamvanchinh203@gmail.com>
2025-11-24 18:38:35 +0700

    add new branch

--- THI_HANG_A1/Models/Xe.cs
+++ THI_HANG_A1/Models/Xe.cs
@@ -8,8 +8,8 @@
         public int GiaiDoan { get; set; }
         public void send()
         {
-// this id devalop branch
-            
+            // this id feature branch
+
         }
     }
 }
