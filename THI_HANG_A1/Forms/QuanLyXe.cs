5cc0a78fa4d9da7732c32805113fbb105f877177
PhamChinhCode <phamvanchinh203@gmail.com>
2025-11-26 21:14:09 +0700

    fixx socket

---
+++ THI_HANG_A1/Forms/QuanLyXe.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using THI_HANG_A1.Models;
+
+namespace THI_HANG_A1.Forms
+{
+    public partial class QuanLyXe : Form
+    {
+        private List<Moto> xes;
+        public QuanLyXe(List<Moto> x)
+        {
+            InitializeComponent();
+            xes = x;
+        }
+    }
+}
